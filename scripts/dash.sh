d6267fea53bc9f1ce57a3f7e3c809818ddc745c8
Luke Cesarz <lucesarz@gmail.com>
2018-04-26 00:33:02 -0500

    dashboard news stuff

--- scripts/dash.sh
+++ scripts/dash.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
-curl wttr.in/minneapolis > output.html
-clear
-cat output.html
+COUNT=0
+HEADLINE_COUNT=0
+I=0
+J=0
 
+while true; do
+    if [ $COUNT == 2 ] 
+        then
+            curl -s wttr.in/minneapolis > weather.html
+    fi
+    head -n -2 weather.html
 
-sleep 30
+    if [ $COUNT == 2 ] 
+        then
+            curl -s http://feeds.bbci.co.uk/news/rss.xml | grep "<title>" | sed "s/<title><\!\[CDATA\[//g;s/\]\]><\/title>//;" | grep -v "BBC News" > news.html
+            COUNT=0
+            HEADLINE_COUNT="$(wc -l news.html| cut -d " " -f 1)"
+            echo "${HEADLINE_COUNT}"
+    fi
+    
+    echo "nnnn  nnnnnnnn        eeeeeeeeeeee  wwwwwww           wwwww           wwwwwww  ssssssssss          "
+    echo "n:::nn::::::::nn    ee::::::::::::ee w:::::w         w:::::w         w:::::w ss::::::::::s   ::::::"
+    echo "n::::::::::::::nn  e::::::eeeee:::::eew:::::w       w:::::::w       w:::::wss:::::::::::::s  ::::::"
+    echo "nn:::::::::::::::ne::::::e     e:::::e w:::::w     w:::::::::w     w:::::w s::::::ssss:::::s ::::::"
+    echo "  n:::::nnnn:::::ne:::::::eeeee::::::e  w:::::w   w:::::w:::::w   w:::::w   s:::::s  ssssss        "
+    echo "  n::::n    n::::ne:::::::::::::::::e    w:::::w w:::::w w:::::w w:::::w      s::::::s             "
+    echo "  n::::n    n::::ne::::::eeeeeeeeeee      w:::::w:::::w   w:::::w:::::w          s::::::s          "
+    echo "  n::::n    n::::ne:::::::e                w:::::::::w     w:::::::::w     ssssss   s:::::s  ::::::"
+    echo "  n::::n    n::::ne::::::::e                w:::::::w       w:::::::w      s:::::ssss::::::s ::::::"
+    echo "  n::::n    n::::n e::::::::eeeeeeee         w:::::w         w:::::w       s::::::::::::::s  ::::::"
+    echo "  n::::n    n::::n  ee:::::::::::::e          w:::w           w:::w         s:::::::::::ss         "
+    echo "  nnnnnn    nnnnnn    eeeeeeeeeeeeee           www             www           sssssssssss           "
+    echo ""
+    echo ""
 
-clear
-
-curl -s http://feeds.bbci.co.uk/news/rss.xml | grep "<title>" | sed "s/ <title><\!\[CDATA\[//g;s/\]\]><\/title>//;" | grep -v "BBC News"
-
-sleep 30
-
-clear
+    sleep 10
+    clear
+    ((COUNT+=1))
+done
