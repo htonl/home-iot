cd4ed70b800e631ba71677074b438d879cfa63c6
bviza44 <bvizanko44@gmail.com>
2018-04-29 14:44:08 -0500

    Made some real dank changes to the script.

--- scripts/dash.sh
+++ scripts/dash.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
-curl wttr.in/minneapolis > output.html
-clear
-cat output.html
+echo '                                     /$$     /$$' > output.html
+echo '                                    | $$    | $$' >> output.html
+echo ' /$$  /$$  /$$  /$$$$$$   /$$$$$$  /$$$$$$  | $$$$$$$   /$$$$$$   /$$$$$$' >> output.html
+echo '| $$ | $$ | $$ /$$__  $$ |____  $$|_  $$_/  | $$__  $$ /$$__  $$ /$$__  $$' >> output.html
+echo '| $$ | $$ | $$| $$$$$$$$  /$$$$$$$  | $$    | $$  \ $$| $$$$$$$$| $$  \__/' >> output.html
+echo '| $$ | $$ | $$| $$_____/ /$$__  $$  | $$ /$$| $$  | $$| $$_____/| $$' >> output.html
+echo '|  $$$$$/$$$$/|  $$$$$$$|  $$$$$$$  |  $$$$/| $$  | $$|  $$$$$$$| $$' >> output.html
+echo ' \_____/\___/  \_______/ \_______/   \___/  |__/  |__/ \_______/|__/' >> output.html
+printf "\n" >> output.html
 
-sleep 30
+curl -s wttr.in/minneapolis >> output.html
+head -n -2 output.html > noutput.html
+echo ' /$$$$$$$   /$$$$$$  /$$  /$$  /$$  /$$$$$$$' >> noutput.html
+echo '| $$__  $$ /$$__  $$| $$ | $$ | $$ /$$_____/' >> noutput.html
+echo '| $$  \ $$| $$$$$$$$| $$ | $$ | $$|  $$$$$$' >> noutput.html
+echo '| $$  | $$| $$_____/| $$ | $$ | $$ \____  $$' >> noutput.html
+echo '| $$  | $$|  $$$$$$$|  $$$$$/$$$$/ /$$$$$$$' >> noutput.html
+echo '|__/  |__/ \_______/ \_____/\___/ |_______/' >> noutput.html
+printf "\n" >> noutput.html
+printf "\n" >> noutput.html
 
-clear
+curl -s http://feeds.bbci.co.uk/news/rss.xml | grep "<title>" | sed "s/ <title><\!\[CDATA\[//g;s/\]\]><\/title>//;" | grep -v "BBC News" >> news.txt
 
-curl -s http://feeds.bbci.co.uk/news/rss.xml | grep "<title>" | sed "s/ <title><\!\[CDATA\[//g;s/\]\]><\/title>//;" | grep -v "BBC News"
+declare -a newsarray
 
-sleep 30
+readarray newsarray < news.txt
 
-clear
+while true; do
+
+i = 0
+while (( ${#newsarray[@]} > i )); do   
+    echo ${newsarray[i++]} >> noutput.html
+    printf "\n" >> noutput.html
+    echo ${newsarray[i++]} >> noutput.html
+    printf "\n" >> noutput.html
+    echo ${newsarray[i++]} >> noutput.html
+    printf "\n" >> noutput.html
+    echo ${newsarray[i++]} >> noutput.html
+    printf "\n" >> noutput.html
+    clear
+    cat noutput.html
+    sleep 10
+    head -n -8 noutput.html > temp.html
+    cat temp.html > noutput.html
+done
+
+done
